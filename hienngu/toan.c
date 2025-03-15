eebf2a48f7c43fc5c470567ed1259fdf952431b5
cntlmh <nguhien.pq@gmail.com>
2025-03-16 05:34:33 +0700

    Add project files.

---
+++ hienngu/toan.c
@@ -0,0 +1,41 @@
+﻿#include<stdio.h>
+/*
+Chức năng: Cộng 2 số nguyên
+Input:
+	int a, int b là 2 số nguyên bất kỳ
+Output: Trả về tổng của a và b
+*/
+
+int cong(int a, int b) {
+	return a+b;
+}
+
+/*
+Chức năng: Trừ 2 số nguyên
+Input:
+	int a, int b là 2 số nguyên bất kỳ
+Output: Trả về hiệu của a và b
+*/
+int tru(int a, int b) {
+	return a - b;
+}
+
+/*
+Chức năng: Nhân 2 số nguyên
+Input:
+	int a, int b là 2 số nguyên bất kỳ
+Output: Trả về tích của a và b
+*/
+int nhan(int a, int b) {
+	return a * b;
+}
+
+/*
+Chuc nang: Chia 2 so nguyen
+Input:
+	int a, int b la 2 so nguyen bat ky
+Output: Tra ve thuong cua a va b
+*/
+float chia(int a, int b) {
+	return (float)a / b;
+}
