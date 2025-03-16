c45aa9f3b1ecd6bd1ca9d8e5f4036105a267095b
cntlmh <nguhien.pq@gmail.com>
2025-03-16 09:51:37 +0700

    In chu viet hoa

--- hienngu/chuoi.c
+++ hienngu/chuoi.c
@@ -1,4 +1,4 @@
-#include <stdint.h>
+﻿#include <stdint.h>
 
 void dem_so_ky_tu(char str[])
 {
@@ -8,4 +8,20 @@ void dem_so_ky_tu(char str[])
 		pos++;
 	}
 	printf("Co %d ky tu trong chuoi %s\r\n", pos + 1, str);
+}
+
+void in_chu_viet_hoa(char str[])
+{
+	int pos = 0;
+	printf("In chu hoa only trong chuoi: %s\r\n",str);
+	while (str[pos] != 0)
+	{
+		
+		if ((str[pos] >= 65) && (str[pos] <= 90))
+		{
+			printf("%c",str[pos]);
+		}
+		pos++;
+	}
+	printf("\r\n");
 }
--- hienngu/chuoi.h
+++ hienngu/chuoi.h
@@ -1,2 +1,3 @@
 #pragma once
 void dem_so_ky_tu(char str[]);
+void in_chu_viet_hoa(char str[]);
